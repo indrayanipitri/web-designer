51d5a46dd63aaf166ff4cd05fbc21b3887bde2bc
Pitri Indrayani <indrayanipitri@gmail.com>
2023-01-17 23:47:09 +0700

    update

--- src/Component/Portfolio/BurgerEnak.js
+++ src/Component/Portfolio/BurgerEnak.js
@@ -6,11 +6,11 @@ export default function Meppsimo() {
   return (
     <div className="background-burgerenak">
         <div className='container-projects' id='portfolio'>
-        <h2>Website Belanja Online - Meppsimo</h2>
+        <h2>Website Burger Enak</h2>
             <div className="text-projects">
                 <div className="subtitle-text">
                     <p>Karena suka baget sama burger, muncul lah ide bikin web landing page ini. Dengan menerapkan prinsip swiss style, jelas design ui nya terlihat simple dan objektif tentunya.</p>
-                    <a href="/" target="_blank" rel="noreferrer"><button>Lihat Web</button></a>
+                    <a href="https://burger-enak.netlify.app/" target="_blank" rel="noreferrer"><button>Lihat Web</button></a>
                     
                 </div>
                 <div className="properties-text">
